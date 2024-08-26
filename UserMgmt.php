8eaf641572456c09a509ff2b1be1ded4190fde30
Anirudha Basu Thakur <122007729+GBCODEZ@users.noreply.github.com>
2024-08-26 16:47:23 +0530

    UserMgmt

--- UserMgmt.php
+++ UserMgmt.php
@@ -1,52 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
     <link rel="stylesheet" href="/php/css/bootstrap.4.4.1.css">
-    <style>
-        table {
-            width: 75%; 
-            margin: 5px auto;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 5px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        td.button-cell {
-            padding: 5px;
-            text-align: center; 
-        }
-        h1{
-            text-align: center;
-        }
-        .button-container {
-            margin-top: 5px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-        .left-buttons {
-            display: flex;
-        }
-        .left-buttons button {
-            margin-right: 10px;
-        }
-    </style>
+    <link rel="stylesheet" href="/php/css/usmp.css">
 </head>
 <body>
-    <h1 class="center">User Details</h1>
+    <h1>User Details</h1>
     <div class="table-container">
         <table>
             <thead>
                 <tr>
-                    <!--<th>Select</th>-->
                     <th>User Name</th>
                     <th>Location</th>
                     <th>Email</th>
@@ -58,15 +24,9 @@
             </thead>
             <tbody>
                 <?php
-                    /*$host = 'localhost';
-                    $db_name = 'college';
-                    $username = 'root';
-                    $password = 'sasa';
-                    $port = '3306';*/
-
                     require 'config.php';
 
-                    $NoRecordsFound = false;
+                    $NoRecordsFound = false; $NoOfRows = 0;
                     $conn = null; $SQL = null; $result = null; $data = null;
                     
                     $conn = new mysqli($host, $username, $password, $db_name, $port);
@@ -75,6 +35,7 @@
                     $data = $result->fetch_all(MYSQLI_ASSOC);
                     if($data)
                     {
+                        $NoOfRows = $result -> num_rows;
                         foreach($data as $row)
                         {
                 ?>                         
@@ -85,7 +46,8 @@
                                 <td><span><?php echo $row['USER_DOB']; ?></span></td>
                                 <td><span><?php echo $row['USER_TYPE']; ?></span></td>
                                 <td><span><?php echo $row['ACTIVE']; ?></span></td>
-                                <td style='text-align:center; vertical-align:middle'><button onclick="window.location.href='UserAction.php?MODE=V&USERID=<?php echo $row['USER_ID']; ?>'
+                                <td style='text-align:center; vertical-align:middle'>
+                                    <button onclick="window.location.href='UserAction.php?MODE=V&USERID=<?php echo $row['USER_ID']; ?>'
                                                     <?php echo $NoRecordsFound ? 'disabled': ''; ?>;">Modify User</button></td>
                             </tr>
                 <?php
@@ -104,19 +66,25 @@
                     }
                     $conn->close(); $conn = null; $SQL = null; $result = null; $data = null;
                 ?>  
-                    <tr>
-                        <td colspan="7">
-                            <div class="button-container">
-                                <div class="left-buttons">
-                                <button onclick="window.location.href='UserAction.php?MODE=I';">Add New User</button>
-                                </div>
-                                <button onclick="window.close();">Cancel</button>
-                            </div>
-                        </td>
-                    </tr>
+                    </tbody>
+            </table>
+    </div>
+    <div>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="width:11%">
+                        <button onclick="window.location.href='UserAction.php?MODE=I';">Add New User</button>
+                    </td>
+                    <td style='width:100%; text-align: center;'>
+                        <label for="nor">No. of Records : <?php echo $NoOfRows; ?></label>
+                    </td>
+                    <td style='width:10%; text-align:right;'>
+                        <button onclick="window.close();">Cancel</button>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
-    <input type="hidden" id="chk_USERID" value=''/> 
 </body>
 </html>
